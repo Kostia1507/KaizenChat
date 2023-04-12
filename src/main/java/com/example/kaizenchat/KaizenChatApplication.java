e1484f042f9650d45a0e8d29005da12ac764944d
Kostia1507 <90578275+Kostia1507@users.noreply.github.com>
2023-04-12 08:17:24 +0300

    setup

---
+++ src/main/java/com/example/kaizenchat/KaizenChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.kaizenchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaizenChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaizenChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kaizenchat/KaizenChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kaizenchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaizenChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
