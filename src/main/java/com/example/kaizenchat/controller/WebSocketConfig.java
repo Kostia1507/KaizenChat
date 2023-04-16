ece6699564d4a8de7d208b0d3f48979ea359d29f
Kostia1507 <90578275+Kostia1507@users.noreply.github.com>
2023-04-16 19:46:27 +0300

    build:add dependecies for web sockets

---
+++ src/main/java/com/example/kaizenchat/controller/WebSocketConfig.java
@@ -0,0 +1,12 @@
+package com.example.kaizenchat.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+
+}
