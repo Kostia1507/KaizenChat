4035f15746588964acafcfbcd497695e163b7cda
Kostia1507 <90578275+Kostia1507@users.noreply.github.com>
2023-04-17 20:02:51 +0300

    feat: create group chat options entity

---
+++ src/main/java/com/example/kaizenchat/entity/GroupChatOptionsEntity.java
@@ -0,0 +1,32 @@
+package com.example.kaizenchat.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "group_chat_options")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GroupChatOptionsEntity {
+
+    @Id
+    @Column(name = "chat_id")
+    private Long chatId;
+
+    @Column(name = "is_private")
+    private Boolean isPrivate;
+
+    private String password;
+
+    @Column(name = "members_limit")
+    private Integer membersLimit;
+
+    @Column(name = "members_count")
+    private Integer membersCount;
+
+}
