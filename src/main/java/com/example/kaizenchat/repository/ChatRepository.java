e00bdca4e3fdf1c81fb06873d359d47cc4350099
Kostia1507 <90578275+Kostia1507@users.noreply.github.com>
2023-04-18 15:21:26 +0300

    feat: create chat repository

--- src/main/java/com/example/kaizenchat/repository/ChatRepository.java
+++ src/main/java/com/example/kaizenchat/repository/ChatRepository.java
@@ -1,6 +1,6 @@
 package com.example.kaizenchat.repository;
 
-import com.example.kaizenchat.entity.MessageEntity;
+import com.example.kaizenchat.entity.ChatEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface MessageRepository extends JpaRepository<MessageEntity, Long> {
+public interface ChatRepository extends JpaRepository<ChatEntity, Long> {
 
 }
