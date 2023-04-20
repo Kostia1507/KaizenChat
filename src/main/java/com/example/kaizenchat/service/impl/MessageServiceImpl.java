6082c332bbcb8203cc11293d18121da31638bdc4
Kostia1507 <90578275+Kostia1507@users.noreply.github.com>
2023-04-20 20:48:59 +0300

    feat(MessageService): add method for deleting messages

--- src/main/java/com/example/kaizenchat/service/impl/MessageServiceImpl.java
+++ src/main/java/com/example/kaizenchat/service/impl/MessageServiceImpl.java
@@ -7,6 +7,7 @@ import com.example.kaizenchat.exception.*;
 import com.example.kaizenchat.repository.ChatRepository;
 import com.example.kaizenchat.repository.MessageRepository;
 import com.example.kaizenchat.repository.UserRepository;
+import com.example.kaizenchat.service.ChatService;
 import com.example.kaizenchat.service.MessageService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 
@@ -25,13 +27,15 @@ public class MessageServiceImpl implements MessageService {
     private final MessageRepository messageRepository;
     private final ChatRepository chatRepository;
     private final UserRepository userRepository;
+    private final ChatService chatService;
 
     @Autowired
     public MessageServiceImpl(MessageRepository messageRepository, ChatRepository chatRepository,
-                              UserRepository userRepository){
+                              UserRepository userRepository, ChatService chatService){
         this.messageRepository = messageRepository;
         this.chatRepository = chatRepository;
         this.userRepository = userRepository;
+        this.chatService = chatService;
     }
 
     public Optional<MessageEntity> findMessageById(Long id){
@@ -65,6 +69,15 @@ public class MessageServiceImpl implements MessageService {
         if(message.getSender().equals(user)){
             message.setBody(body);
             messageRepository.save(message);
+        }else throw new UserViolationPermissionsException();
+    }
+
+    public void deleteMessageById(Long messageId, Long userId)
+            throws MessageNotFoundException, UserViolationPermissionsException {
+        MessageEntity message = messageRepository.findById(messageId).orElseThrow(MessageNotFoundException::new);
+        if(Objects.equals(message.getSender().getId(), userId)
+                || chatService.isUserAdminInGroupChat(message.getSender(), message.getChat())){
+            messageRepository.delete(message);
         }else throw new UserViolationPermissionsException();
     }
 
