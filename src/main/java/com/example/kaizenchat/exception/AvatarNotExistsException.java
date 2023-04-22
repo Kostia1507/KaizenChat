5a19d81832f8314516e19a3778fbf61c105594aa
Igor <ig.zuban1ch@gmail.com>
2023-04-22 14:37:50 +0300

    feat: add exception for non-existent avatar

---
+++ src/main/java/com/example/kaizenchat/exception/AvatarNotExistsException.java
@@ -0,0 +1,10 @@
+package com.example.kaizenchat.exception;
+
+public class AvatarNotExistsException extends Exception {
+    public AvatarNotExistsException() {
+    }
+
+    public AvatarNotExistsException(Throwable cause) {
+        super(cause);
+    }
+}
