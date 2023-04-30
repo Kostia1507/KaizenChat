9144e63f3fc7290ad37380ef7a8c9c0fffe46310
Igor <ig.zuban1ch@gmail.com>
2023-04-30 15:00:47 +0300

    feat: create validation handler for request dtos

---
+++ src/main/java/com/example/kaizenchat/dto/DTOValidationHandler.java
@@ -0,0 +1,24 @@
+package com.example.kaizenchat.dto;
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@RestControllerAdvice
+public class DTOValidationHandler {
+
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return errors;
+    }
+
+}
