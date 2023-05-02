723e23114afe3d324fcfb5b1dc3bad93708fa7e4
Igor <ig.zuban1ch@gmail.com>
2023-05-02 19:41:03 +0300

    feat: add method to handle exceptions related to forbidden response code

--- src/main/java/com/example/kaizenchat/exception/CustomExceptionHandler.java
+++ src/main/java/com/example/kaizenchat/exception/CustomExceptionHandler.java
@@ -2,11 +2,13 @@ package com.example.kaizenchat.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static java.time.ZonedDateTime.now;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Slf4j
@@ -21,8 +23,18 @@ public class CustomExceptionHandler {
             AvatarNotExistsException.class
     })
     public ApiError handleException(Exception e, HttpServletRequest request) {
-        log.error("IN CustomExceptionHandler ->  handleException(): path={} , {}", request.getRequestURI(), e.getMessage());
+        log.error("IN CustomExHandler [404] ->  handleEx(): path={} , {}", request.getRequestURI(), e.getMessage());
         return new ApiError(request.getRequestURI(), e.getMessage(), NOT_FOUND.value(), now());
+    }
+
+    @ResponseStatus(FORBIDDEN)
+    @ExceptionHandler({
+            BadCredentialsException.class,
+            InvalidRequestDataException.class
+    })
+    public ApiError handleExceptionWithForbidden(Exception e, HttpServletRequest request) {
+        log.error("IN CustomExHandler [403] ->  handleExWithForbidden(): path={} , {}", request.getRequestURI(), e.getMessage());
+        return new ApiError(request.getRequestURI(), e.getMessage(), FORBIDDEN.value(), now());
     }
 
 }
