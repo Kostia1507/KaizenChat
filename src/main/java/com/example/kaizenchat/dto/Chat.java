76db0e87aea92fc0bb37d01e57621113def78ccf
Igor <ig.zuban1ch@gmail.com>
2023-05-15 21:32:24 +0300

    refactor(Chat): remove odd annotations, add builder and admin id field

--- src/main/java/com/example/kaizenchat/dto/Chat.java
+++ src/main/java/com/example/kaizenchat/dto/Chat.java
@@ -1,18 +1,19 @@
 package com.example.kaizenchat.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.ZonedDateTime;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
+@Builder
 public class Chat {
 
     private long id;
     private String name;
+    private Long adminId;
     private Long userId;
     private String username;
     private String lastMessage;
